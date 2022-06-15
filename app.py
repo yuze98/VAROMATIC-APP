6721e312ba45ad566543110101d2579e5d40bcfd
Youssef Hatem <65419885+yuze98@users.noreply.github.com>
2022-06-15 20:20:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def window():
+# def window():
+#     app = QApplication(sys.argv)
+#     win = QMainWindow()
+#     win.setGeometry(200,200,300,300)
+#     win.setWindowTitle('Varomatic APP')
+
+#     win.show()
+#     sys.exit(app.exec_())
+
+# window()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.centralwidget = QWidget()
+        self.setCentralWidget(self.centralwidget)
+
+        self.pushButton1 = QPushButton("Button 1", self.centralwidget)
+        self.pushButton2 = QPushButton("Button 2", self.centralwidget)
+
+        lay = QHBoxLayout(self.centralwidget)
+        lay.addWidget(self.pushButton1)
+        lay.addWidget(self.pushButton2)
+
+
+stylesheet = """
+    MainWindow {
+        background-image: url("D:/UNI STUFF\/fourth year/sem 2/GGP/VAROMATIC-APP/assets/logo.jpg"); 
+        background-repeat: no-repeat; 
+        background-position: top;
+    }
+"""
+
+if __name__ == "__main__":
+    import sys
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200,200,300,300)
-    win.setWindowTitle('Varomatic APP')
-
-    win.show()
+    app.setStyleSheet(stylesheet)     # <---
+    window = MainWindow()
+    window.resize(640, 640)
+    window.show()
     sys.exit(app.exec_())
-
-window()
